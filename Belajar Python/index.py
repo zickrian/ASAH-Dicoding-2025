5cda5e6615c9184368b7624956dc336062405397
FirdausKz <espejodaniel50@gmail.com>
2025-08-22 23:56:47 +0700

    first commit

---
+++ Belajar Python/index.py
@@ -0,0 +1,15 @@
+"""
+INI ADALAH BLOK KOMEN
+"""
+multiple = ''' #bisa menulis multiple string
+Haii!!!
+Apa kabar kalian
+Aku baik baik saja 
+'''
+
+print(multiple)
+name = input('Masukan nama Anda: ')
+age = input('Masukan Umur anda: ')
+where = input('Masukan asal anda: ')
+
+print(name, age, where)
