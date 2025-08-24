d965b3f6f8b5d436a1ef6356c4161c620ba808cf
FirdausKz <espejodaniel50@gmail.com>
2025-08-24 14:51:25 +0700

    Add operation.py with examples of list, set, string operations, and their methods

---
+++ Belajar Python/operation.py
@@ -0,0 +1,39 @@
+#Operasi pada List, Set, dan String
+
+#len() menghitung panjang
+list = [1,2,3,4,5,6,7]
+print(list)
+print(len(list))
+
+set = {1,2,2,4,5,1,3,7,8,9,4} #set secara otomatis menghilangkan duplikat 
+print(set)
+print(len(set))
+
+#min dan max
+grade = [50,60,75,85,90,99]
+print(min(grade)) #terkecil
+print(max(grade)) #terbesar
+
+#count
+platform = "Dicoding"
+print(platform.count("i")) #menghitung jumlah i
+
+print(list.count(4)) #menghitung angka 4 di dalam list, count tidak bisa di lakukan pada set 
+
+tuple = (88,44, 5, 44, 44, 49)
+print(tuple.count(44))
+
+#in dan not in
+
+university = "dian nuswantoro university"
+print('dian' in university) #true
+
+print('semarang ' not in university) #true
+
+#short ( mengurutkan)
+trans = ["bandung", "semarang", "jakarta", "surabaya"]
+trans.sort()
+print(trans)
+
+trans.sort(reverse=True)
+print(trans)
