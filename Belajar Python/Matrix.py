ecc672826bac5d95b2987714040a84fdcc3f0aea
FirdausKz <espejodaniel50@gmail.com>
2025-08-30 17:33:07 +0700

    Refactor Matrix.py to use 'numpy as np' for consistency and add matrix multiplication examples with manual and NumPy methods in subprogram.py

--- Belajar Python/Matrix.py
+++ Belajar Python/Matrix.py
@@ -1,6 +1,6 @@
 #di dalam matriks kita dapat menggunakan list biasa dan juga library numpy
 
-import numpy
+import numpy as np
 import sys #mengecek bit yang di gunakan
 
 list = [[1,2,3],[4,5,6],[7,8,9]]
@@ -9,7 +9,7 @@ for i in list:
 
 #mencoba menggunakan numpy
 
-matriks = numpy.array([[1,2,3],[4,5,6],[7,8,9]])
+matriks = np.array([[1,2,3],[4,5,6],[7,8,9]])
 print('Jumlah Bit yang di gunakan pada Matriks = ', matriks.size * matriks.itemsize) #numpy menggunakan 72 bit 
 print('Jumlah Bit yang di gunakan pada List = ', sys.getsizeof(list * len(list))) #list biasa menghabiskan 128 bit
 
@@ -45,7 +45,7 @@ matriks1 = [[1,2,3], #0
     [3,6,9]
 
 '''
-var_1 = matriks1[1][2]
+var_1 = matriks1[2][1]
 print(var_1)
 
 matriks1[1][2]=9
@@ -55,4 +55,31 @@ print(matriks1)
 print(matriks1[0][2])
 #[[1, 2, 3], [4, 5, 9], [7, 8, 9]]
 #jadi 0 itu berari beradaa di matriks pertama [1,2,3] lalu 2 itu di dalam matriks pertama yakni 3
+
+
+
+#cara menghitung perkalian dengan matriks tanpa numpy
+
+var = [[1,2,3 ],
+       [4,5,6]]
+
+var_result= [[0 for j in range(3)] for i in range(2)]
+
+for i in range(len(var)): #len var berisi 0 dan juga 1
+    for j in range(len(var[0])): #len var[0] berisi 0 1 2 
+        var_result[i][j] = var[i][j]*2
+
+print(var_result) 
+
+#cara menggunakan numpy
+
+var_before = np.array( [[1,2,3],[4,5,6]])
+
+result =  var_before * 2
+print(result)
+
+#cek bits
+print("ukuran bit dari result menggunakan matrix Pake Numpy: ", var_before.size * var_before.itemsize )
+print("ukuran bit dari result menggunakan matrix Manual pake List: ", sys.getsizeof(var * len(var)))
+
 
---
+++ Belajar Python/subprogram.py
@@ -0,0 +1,66 @@
+# fungsi dalam python
+
+# Fungsi untuk menghitung luas persegi panjang dengan parameter panjang dan lebar
+def luas_persegi_panjang(panjang, lebar):
+    result  = panjang * lebar  # Menghitung hasil perkalian panjang dan lebar
+    return result  # Mengembalikan hasil perhitungan
+
+# Memanggil fungsi dengan positional argument (berdasarkan urutan parameter)
+persegi_panjang1 = luas_persegi_panjang(10, 2)  # Menghitung luas persegi panjang dengan panjang=10 dan lebar=2
+print(persegi_panjang1)  # Menampilkan hasil perhitungan
+
+# Memanggil fungsi dengan keyword argument (menyebutkan nama parameter secara eksplisit)
+print("hasil dari persegi panjang2 adalah: ", luas_persegi_panjang(lebar=10, panjang=3))  # Menghitung luas dengan parameter terbalik
+
+
+# Fungsi dengan parameter positional atau keyword
+def greeting(nama, pesan):
+    return "Halo, " + nama + "! " + pesan  # Mengembalikan pesan sapaan
+
+# Memanggil fungsi dengan positional argument
+print(greeting("Dicoding", "Selamat pagi!"))  # Menampilkan sapaan dengan nama dan pesan
+
+# Memanggil fungsi dengan keyword argument
+print(greeting(pesan="Selamat sore!", nama="Dicoding"))  # Menampilkan sapaan dengan parameter terbalik
+
+
+# Fungsi dengan parameter positional-only (hanya bisa dipanggil berdasarkan urutan)
+def penjumlahan(a, b, /):
+    return a + b  # Mengembalikan hasil penjumlahan dua angka
+
+# Memanggil fungsi dengan positional argument
+print(penjumlahan(3, 5))  # Menampilkan hasil penjumlahan 3 + 5
+
+
+# Fungsi dengan parameter keyword-only (hanya bisa dipanggil dengan menyebutkan nama parameter)
+def greeting(*, nama, pesan):
+    return "Halo, " + nama + "! " + pesan  # Mengembalikan pesan sapaan
+
+# Memanggil fungsi dengan keyword argument
+print(greeting(pesan="Selamat sore!", nama="Dicoding"))  # Menampilkan sapaan dengan nama dan pesan
+
+
+# Fungsi dengan variadic positional arguments (*args)
+def hitung_total(*args):
+    print(type(args))  # Menampilkan tipe data args (tuple)
+    total = sum(args)  # Menghitung total dari semua angka dalam args
+    return total  # Mengembalikan hasil penjumlahan
+
+# Memanggil fungsi dengan sejumlah angka sebagai argumen
+print(hitung_total(1, 2, 3))  # Menampilkan total dari 1 + 2 + 3
+
+
+# Fungsi dengan variadic keyword arguments (**kwargs)
+def cetak_info(**kwargs):
+    info = ""  # Inisialisasi string kosong untuk menyimpan informasi
+    for key, value in kwargs.items():  # Iterasi melalui pasangan key-value dalam kwargs
+        info += key + ': ' + value + ", "  # Menambahkan informasi ke string
+    return info  # Mengembalikan string informasi
+
+# Memanggil fungsi dengan sejumlah pasangan key-value
+print(cetak_info(nama="Dicoding", usia="17", pekerjaan="Python Programmer"))  # Menampilkan informasi
+
+
+# Fungsi lambda untuk operasi pertambahan
+pertambahan = lambda sisi, hasil: sisi + sisi  # Fungsi lambda untuk menambahkan sisi dua kali
+print(pertambahan(10, 10))  # Menampilkan hasil pertambahan 10 + 10
